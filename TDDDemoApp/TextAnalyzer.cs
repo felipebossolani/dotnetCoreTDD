91e7e1f908608742ca0030ab77e30d5f67aa9ee3
Felipe Bossolani <fbossolani@gmail.com>
2018-09-23 17:07:51 -0300

    Adding .net solution

---
+++ TDDDemoApp/TextAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDDemoApp
+{
+    public class TextAnalyzer
+    {
+        public int GetTotalWordCount(string text)
+        {
+            int wordCount = 0, index = 0;
+
+            while (index < text.Length)
+            {
+                // check if current char is part of a word
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            return wordCount;
+        }
+    }
+}
---
+++ TDDDemoAppTests/TextAnalyzerUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using TDDDemoApp;
+using Xunit;
+
+namespace TDDDemoAppTests
+{
+    public class TextAnalyzerUnitTests
+    {
+        [Fact]
+        public void GetTotalWordCount_WhenPassedRegularText_ShouldReturnExpectedCount()
+        {
+            // arrange
+            const string textToAnalyze = @"xUnit.net is a free, open source, community-focused
+                                            unit testing tool for the .NET Framework.";
+            var analyzer = new TextAnalyzer();
+
+            // act 
+            var count = analyzer.GetTotalWordCount(textToAnalyze);
+
+            // assert
+            Assert.Equal(14, count);
+        }
+    }
+}
